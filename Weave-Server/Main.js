03b1276654a2acf6ee7bf1bbadf1c2c68eae33c3
Christopher Tse <christopher-tse@outlook.com>
2017-03-16 22:17:00 -0500

    Create Weave server file
    Includes a function to take data upon invite and logs out 'x has invited y'

---
+++ Weave-Server/Main.js
@@ -0,0 +1,23 @@
+const server = require('http').createServer();
+const io = require('socket.io')(server);
+const port = 8080;
+
+
+io.on('connection', client => {
+	console.log('Client connected...');
+
+	// Connection listener
+	// Mostly just for debug purposes
+	client.on('join', data => {
+    console.log(data);
+	});
+
+	// Invite listener, logs out 'x has invited y'
+	// Depending on what is needed, maybe send
+	// an array of usernames and IDs 
+	client.on('invite', data => {
+		console.log(data);
+	})
+})
+
+server.listen(port);
